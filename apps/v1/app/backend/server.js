651c57dea82f0e71a9a9b4c29c4ccf74a27d3e7b
WebMakaka <marley.nodejs@gmail.com>
2021-02-09 18:25:36 +0300

    metrics works

--- apps/v1/app/backend/server.js
+++ apps/v1/app/backend/server.js
@@ -1,42 +1,71 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-var mongodb = require('mongodb');
+const mongodb = require('mongodb');
 
-var MONGODB_URI = process.env["MONGODB_URI"];
+const MONGODB_URI = process.env['MONGODB_URI'];
 
-var mongoClient = mongodb.MongoClient;
+const mongoClient = mongodb.MongoClient;
+const client = require('prom-client');
 
+// Create a Registry which registers the metrics
+const register = new client.Registry();
+// Add a default label which is added to all metrics
+register.setDefaultLabels({
+  app: 'prometheus-nodejs-app',
+});
+// Enable the collection of default metrics
+client.collectDefaultMetrics({ register });
 
 var collection;
-var db = mongoClient.connect(MONGODB_URI, {
-  autoReconnect: true,
-  reconnectInterval: 10000,
-  reconnectTries: Number.MAX_VALUE,
-  useNewUrlParser: true
-}, function (err, db) {
-  if (err)
-    throw err;
-  console.log("connected to the mongoDB !");
-  collection = db.db('guestbook').collection('greeetings');
-});
+var db = mongoClient.connect(
+  MONGODB_URI,
+  {
+    autoReconnect: true,
+    reconnectInterval: 10000,
+    reconnectTries: Number.MAX_VALUE,
+    useNewUrlParser: true,
+  },
+  function (err, db) {
+    if (err) throw err;
+    console.log('connected to the mongoDB !');
+    collection = db.db('guestbook').collection('greeetings');
+  }
+);
 
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  console.log('use cors')
+  console.log('use cors');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
   next();
 });
 
+app.get('/metrics', async (req, res) => {
+  console.log('Metrics 01');
+  // Return all metrics the Prometheus exposition format
+  res.setHeader('Content-Type', register.contentType);
+  result = await register.metrics();
+  return res.send(result);
+});
+
 app.get('/guestbook', function (req, res) {
-  console.log('Guestbook API GET')
-  collection.find().sort({ _id: -1 }).toArray((err, guestbook) => {
-    res.send(guestbook);
-  })
+  console.log('Guestbook API GET');
+  collection
+    .find()
+    .sort({ _id: -1 })
+    .toArray((err, guestbook) => {
+      return res.send(guestbook);
+    });
 });
 
 app.post('/guestbook', function (req, res) {
@@ -48,10 +77,10 @@ app.post('/guestbook', function (req, res) {
   collection.insert(newGreeting, (err, greeting) => {
     if (err) console.log(err);
     res.send(greeting);
-  })
+  });
 });
 
 app.listen(3000, function () {
   // console.log("MONGODB_URI:"+MONGODB_URI)
-  console.log('Guestbook API listening on port 3000!')
+  console.log('Guestbook API listening on port 3000!');
 });
